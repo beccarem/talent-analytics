41017f549360b1756c14c59944757e02fa8e06c8
rebeccamukena <93835423+beccarem@users.noreply.github.com>
2022-01-24 21:03:40 -0500

    update assignmemt 2

--- code/assignment2.R
+++ code/assignment2.R
@@ -7,34 +7,36 @@ library('arrow')
 library('gender')
 library('wru')
 library('dplyr')
+library('zoo')
 
 # Import data:
 app_data = read_parquet('code/app_data_sample.parquet')
 attach(app_data)
 
-# gender function
-# gender("madison")
-
-# wru
+# wru - change the last name column to surname
 app_data['surname'] = app_data['examiner_name_last']
 attach(app_data)
 
-#predictions = predict_race(voter.file = app_data, surname.only = T)
+# prediction of race percentages - takes a long time to run
+predictions = predict_race(voter.file = app_data, surname.only = T)
 attach(predictions)
 
-final_data <- transform(predictions, pred.race = pmax(pred.whi, pred.bla, 
-                                                      pred.his, pred.asi, 
-                                                      pred.oth))
-attach(final_data)
+# finding the highest probability
+predictions_final <- transform(predictions, pred.race = pmax(pred.whi, pred.bla, pred.his, pred.asi, pred.oth))
+attach(predictions_final)
 
-final_data <- transform(final_data, race = ifelse(pred.race == pred.whi, "white", 
-                                                  ifelse(pred.race == pred.bla, "black", 
-                                                         ifelse(pred.race == pred.his, "hispanic", 
-                                                                ifelse(pred.race == pred.asi, "asian", "other")))))
+# added "race" column
+predictions_final <- transform(predictions_final, race = ifelse(pred.race == pred.whi, "white", 
+                                                                ifelse(pred.race == pred.bla, "black", 
+                                                                      ifelse(pred.race == pred.his, "hispanic", 
+                                                                            ifelse(pred.race == pred.asi, "asian", "other")))))
 
-examiners_byartunit <- final_data %>% group_by(examiner_art_unit, race)
-count_byraceinartunit <- examiners_byartunit %>% tally()
+# added count of examiner by race in art unit
+examiners_byrace_inartunit <- predictions_final %>% group_by(examiner_art_unit, race)
+count_byraceinartunit <- examiners_byrace_inartunit %>% tally()
 
-
+# quarters
+# quart <- as.yearqtr(app_data$filing_date, format = "%Y-%m-%d")
+# quart
 
 
